0a54613556aebbf00162dc28785a6f7bc02ee6c0
Sourabh Jadhav <sourabhj053@gmail.com>
2024-12-15 19:57:38 +0530

    first commit

---
+++ src/test/java/com/example/learningnavigator/LearningnavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningnavigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningnavigatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
