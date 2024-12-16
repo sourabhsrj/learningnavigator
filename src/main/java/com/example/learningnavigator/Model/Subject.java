896bfbecebe36aef170ffe4ee13f6fa6358858fe
Sourabh Jadhav <sourabhj053@gmail.com>
2024-12-16 09:45:54 +0530

    Implemented subject model....!

---
+++ src/main/java/com/example/learningnavigator/Model/Subject.java
@@ -0,0 +1,48 @@
+package com.example.learningnavigator.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Subject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String subjectName;
+
+    @ManyToMany(mappedBy = "enrolledSubjects")
+    private List<Student> registeredStudents = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public List<Student> getRegisteredStudents() {
+        return registeredStudents;
+    }
+
+    public void setRegisteredStudents(List<Student> registeredStudents) {
+        this.registeredStudents = registeredStudents;
+    }
+}
