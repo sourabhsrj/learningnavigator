0f8ce42212902a07ae694adfd56cf3a6ba03df63
Sourabh Jadhav <sourabhj053@gmail.com>
2024-12-16 09:46:03 +0530

    Implemented Student model....!

---
+++ src/main/java/com/example/learningnavigator/Model/Student.java
@@ -0,0 +1,70 @@
+package com.example.learningnavigator.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(unique = true, nullable = false)
+    private String registrationId;
+
+    private String name;
+
+    @ManyToMany
+    private List<Subject> enrolledSubjects = new ArrayList<>();
+
+    @ManyToMany
+    private List<Exam> registeredExams = new ArrayList<>();
+
+      public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRegistrationId() {
+        return registrationId;
+    }
+
+    public void setRegistrationId(String registrationId) {
+        this.registrationId = registrationId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Subject> getEnrolledSubjects() {
+        return enrolledSubjects;
+    }
+
+    public void setEnrolledSubjects(List<Subject> enrolledSubjects) {
+        this.enrolledSubjects = enrolledSubjects;
+    }
+
+    public List<Exam> getRegisteredExams() {
+        return registeredExams;
+    }
+
+    public void setRegisteredExams(List<Exam> registeredExams) {
+        this.registeredExams = registeredExams;
+    }
+}
